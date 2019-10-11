5dbaea4b8ebb37138478b436608137931935e4e5
QElderDelta <egorzhivalev@gmail.com>
2019-10-11 15:23:22 +0300

    deleted build

--- build/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/qelderdelta/Study/oop_exercise_02/googletest/googlemock
-# Build directory: /home/qelderdelta/Study/oop_exercise_02/build/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
